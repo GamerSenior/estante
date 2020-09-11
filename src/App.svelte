6272fd4fb2cda1e7f4f255a635761bbaa566fe9e
Giovani Garcia <abelgiovani@gmail.com>
2020-09-11 19:29:23 -0300

    Primeiro commit

---
+++ src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+	import Menu, {TELAS} from './Menu.svelte'
+	import Home from './views/Home.svelte'
+	import Amigos from './views/Amigos.svelte'
+	import Livros from './views/Livros.svelte'
+	import Opcoes from './views/Opcoes.svelte'
+
+	let telaAtual = TELAS.HOME;
+
+	function mudarTela(evento) {
+		alert("Tela atual: "  + evento.detail)
+		telaAtual = evento.detail
+	}
+</script>
+
+<main>
+	{#if telaAtual === TELAS.HOME}
+	<Home></Home>
+	{:else if telaAtual === TELAS.AMIGOS}
+	<Amigos></Amigos>
+	{:else if telaAtual === TELAS.LIVROS}
+	<Livros></Livros>
+	{:else if telaAtual === TELAS.OPCOES}
+	<Opcoes></Opcoes>
+	{/if}
+
+	<Menu on:mudarTela={mudarTela}></Menu>
+</main>
+
+<style>
+	:global(body) {
+		padding: 0;
+	}
+
+	main {
+		background-color: aqua;
+		height: 100%;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Menu.svelte
@@ -0,0 +1,93 @@
+<div id="container">
+    <div class="sub-container" on:click={() => {trocarTela(TELAS.HOME)}} >
+        <div class="botoes">
+            <div class="icones">
+                <Icon icon={faHome}/>
+            </div>
+            <span>Home</span>
+        </div>
+    </div>
+    <div class="sub-container" on:click={() => trocarTela(TELAS.AMIGOS)}>
+        <div class="botoes">
+            <div class="icones">
+            <Icon icon={faUsers}/>
+        </div>
+        Amigos
+        </div>
+    </div>
+    <div class="sub-container" on:click={() => trocarTela(TELAS.LIVROS)}>
+        <div class="botoes">
+            <div class="icones">
+            <Icon icon={faBook}/>
+        </div>
+        Livros
+        </div>
+    </div>
+    <div class="sub-container" on:click={() => trocarTela(TELAS.OPCOES)}>
+        <div class="botoes">
+            <div class="icones">
+            <Icon icon={faUserCog}/>
+        </div>
+        Opções
+        </div>
+    </div>
+</div>
+
+<script context="module">
+	export const TELAS = {
+		HOME: 1,
+		AMIGOS: 2,
+		LIVROS: 3,
+		OPCOES: 4
+	}
+</script>
+
+<script>
+    import Icon from 'fa-svelte'
+    import { faHome } from '@fortawesome/free-solid-svg-icons/faHome'
+    import { faUsers } from '@fortawesome/free-solid-svg-icons/faUsers'
+    import { faBook } from '@fortawesome/free-solid-svg-icons/faBook'
+    import { faUserCog } from '@fortawesome/free-solid-svg-icons/faUserCog'
+    import { createEventDispatcher } from 'svelte'
+
+    const dispatch = createEventDispatcher()
+
+    function trocarTela(tela) {
+        dispatch('mudarTela', tela)
+    }
+</script>
+
+<style>
+    #container {
+		background-color: darkslategray;
+		width: 100%;
+		height: 5em;
+		position: fixed;
+		left: 0;
+		bottom: 0;
+        display: flex;
+        justify-content: space-around;
+	}
+
+    .sub-container {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        color: whitesmoke;
+    }
+
+    .botoes {
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    .sub-container:hover {
+        background-color: black;
+    }
+
+    .icones {
+        font-size: 30px;
+        text-align: center;
+    }
+</style>
